4f4a42ab05c3d4935a3285b09db8802ce2ed3a20
ALEX LINARDOS <33348399+AlexLinardos@users.noreply.github.com>
2021-03-20 14:53:05 +0200

    Creation of moving_balls.py
    Δημιουργία του αρχείου κώδικα που παρουσιάζει παράδειγμα για την κίνηση μπαλών στο tkinter και την παύση κίνησης όταν φτάσουν σε ένα δεδομένο σημείο.

---
+++ moving_balls.py
@@ -0,0 +1,37 @@
+from tkinter import *
+
+
+class Ball:
+    def __init__(self, canvas, x1, y1, x2, y2, delta_x, delta_y, color):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        self.delta_x = delta_x
+        self.delta_y = delta_y
+        self.canvas = canvas
+        self.color = color
+        self.circle = canvas.create_oval(
+            self.x1, self.y1, self.x2, self.y2, fill=self.color)
+
+    def move_ball(self):
+        if self.canvas.coords(self.circle)[0] <= 250 and self.canvas.coords(self.circle)[1] <= 250:
+            self.canvas.move(self.circle, self.delta_x, self.delta_y)
+            self.canvas.after(50, self.move_ball)
+
+
+# main
+window = Tk()
+window.title("Moving balls")
+window.resizable(False, False)
+
+canvas = Canvas(window, width=300, height=300)
+canvas.pack()
+
+ball1 = Ball(canvas, 10, 10, 30, 30, 5, 5, "red")
+ball2 = Ball(canvas, 60, 60, 80, 80, 4, 4, "green")
+
+ball1.move_ball()
+ball2.move_ball()
+
+window.mainloop()
