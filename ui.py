782db7debb558f653eec0b44c56e341161a78e79
JimmyThePhantom <dim.dalamagkas@gmail.com>
2021-05-08 18:53:06 +0300

    Κουμπιά Start και Pause
    Προσθήκη του κουμπιού start και του κομπιού pause που ξεκινάει την προσομοίωση και την "παγώνει" ή την "ξεπαγώνει" αντίστοιχα.

--- ui.py
+++ ui.py
@@ -1,14 +1,15 @@
 import SimulationEnvironment as se
 from tkinter import *
+from uiFunctions import *
 
-class Ui:
-    ui_elements = []
+# H συνάρτηση που αρχικοποιεί το UI
+def initUI(simulation, window):
+    # Δημιουργία του κουμπιού Start
+    start_button = Button(window, text="Start", command=lambda: start(simulation))
+    start_button.pack()
+    start_button.place(x=simulation.canvas_size[0] - simulation.ui_space + 30, y=simulation.canvas_size[1] // 8)
 
-    def __init__(self, window, simulation):
-        self.window = window
-        self.simulation = simulation
-
-        Ui.ui_elements.append(Button(self.window, text="Start", ))
-
-    def s(self):
-        self.simulation.
+    # Δημιουργία του κουμπιού Pause
+    pause_button = Button(window, text="Pause", command=lambda: pause(simulation, pause_button))
+    pause_button.pack()
+    pause_button.place(x=simulation.canvas_size[0] - simulation.ui_space + simulation.ui_space // 3, y=simulation.canvas_size[1] // 8)
