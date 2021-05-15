d960385e7ca3ce71d4f76645a657a0cf8fc2cce8
JimmyThePhantom <64829159+JimmyThePhantom@users.noreply.github.com>
2021-05-15 15:15:17 +0300

    Κουμπιά Pause, Play και Stop

--- ui.py
+++ ui.py
@@ -1,15 +1,25 @@
 import SimulationEnvironment as se
 from tkinter import *
+from tkinter.ttk import *
 from uiFunctions import *
 
-# H συνάρτηση που αρχικοποιεί το UI
-def initUI(simulation, window):
-    # Δημιουργία του κουμπιού Start
-    start_button = Button(window, text="Start", command=lambda: start(simulation))
-    start_button.pack()
-    start_button.place(x=simulation.canvas_size[0] - simulation.ui_space + 30, y=simulation.canvas_size[1] // 8)
+class Ui:
+    # H συνάρτηση που αρχικοποιεί το UI
+    def __init__(self, simulation, window):
+        # Δημιουργία του κουμπιού Pause και Play
 
-    # Δημιουργία του κουμπιού Pause
-    pause_button = Button(window, text="Pause", command=lambda: pause(simulation, pause_button))
-    pause_button.pack()
-    pause_button.place(x=simulation.canvas_size[0] - simulation.ui_space + simulation.ui_space // 3, y=simulation.canvas_size[1] // 8)
+        self.pause_image = PhotoImage(file = r"images/pause.png")
+        self.play_image = PhotoImage(file = r"images/play.png")
+        self.stop_image = PhotoImage(file = r"images/stop.png")
+
+        self.images = [self.play_image, self.pause_image]
+        self.place = 1
+
+        pause_button = Button(window, text="Play", image=self.play_image, command=lambda: pause(simulation, pause_button, self))
+        pause_button.pack()
+        pause_button.place(x=simulation.canvas_size[0] - simulation.ui_space + 30, y=simulation.canvas_size[1] // 8)
+        
+        # Δημιουργία του κουμπιού Start
+        stop_button = Button(window, text="Stop", image=self.stop_image, command=lambda: stop(simulation))
+        stop_button.pack()
+        stop_button.place(x=simulation.canvas_size[0] - simulation.ui_space + simulation.ui_space // 3, y=simulation.canvas_size[1] // 8)
--- uiFunctions.py
+++ uiFunctions.py
@@ -1,15 +1,23 @@
 from tkinter import *
 
-# Η εντολή που θα εκτελεί το κουμπί Start
-def start(simulation):
-    simulation.run()
-    simulation.has_started = True
+# Η εντολή που θα εκτελεί το κουμπί Stop
+def stop(simulation):
+    simulation.destroy()
 
-# Η εντολή που θα εκτελεί το κουμπί Pause
-def pause(simulation, btn):
+# Η εντολή που θα εκτελεί το κουμπί Pause και Play
+def pause(simulation, btn, ui):
+
+    if not simulation.has_started:
+        btn["image"] = ui.images[ui.place]
+        simulation.run()
+        simulation.has_started = True
+
     simulation.is_paused = not simulation.is_paused
 
-    if btn["text"] == "Pause":
-        btn["text"] = "Unpause"
-    elif btn["text"] == "Unpause":
-        btn["text"] = "Pause"
+    if ui.place == 0:
+        ui.place = 1
+    elif ui.place == 1:
+        ui.place = 0
+
+    btn["image"] = ui.images[ui.place]
+
